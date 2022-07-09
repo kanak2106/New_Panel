e6c65c3dba79e3fb78838ffdd346aee184eae3eb
Kanak Gautam <kanakgautam2106@gmail.com>
2022-07-09 15:12:07 +0530

    first commit

---
+++ src/Components/Pages/layout/navbar.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { Link, NavLink} from "react-router-dom";
+
+const Navbar = () => {
+    return (
+        <nav className="navbar navbar-expand-lg bg-light">
+            <div className="container">
+                <div className="container-fluid">
+                    <a className="navbar-brand" href="#">Admin Panel</a>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                            <li className="nav-item">
+                                <NavLink className="nav-link active" aria-current="page" exact to="/">Home</NavLink>
+                            </li>
+                            <li className="nav-item">
+                                <NavLink className="nav-link" exact to="/about">About</NavLink>
+                            </li>
+                            <li className="nav-item">
+                                <NavLink className="nav-link" exact to="/contact">Contact</NavLink>
+                            </li>
+                           
+                            
+                        </ul>
+                       
+                    </div>
+                </div>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar;
